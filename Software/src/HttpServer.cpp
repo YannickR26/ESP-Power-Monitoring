e9b242182a667e5e31b42c1c1fdbab287344de9e
Yannick RICHARDOT <Yannick.richardot@carbonbee.fr>
2022-04-16 12:20:31 +0200

    change indent of httpServer

--- Software/src/HttpServer.cpp
+++ Software/src/HttpServer.cpp
@@ -5,6 +5,7 @@
 #elif defined(ESP32)
 #include <SPIFFS.h>
 #define FS SPIFFS
+#include <ESPmDNS.h>
 #endif
 
 // You can update by 'curl -F "image=@firmware.bin" ESP_Monitoring.local/'
@@ -15,6 +16,8 @@
 #include "Logger.h"
 #include "Mqtt.h"
 #include "SimpleRelay.h"
+
+#include "web_autogen.h"
 
 extern SimpleRelay relay;
 
@@ -33,282 +36,295 @@ HttpServer::~HttpServer()
 
 void HttpServer::setup(void)
 {
-#if defined(ESP8266)
-  MDNS.begin(Configuration._hostname.c_str());
-  MDNS.addService("http", "tcp", 80);
+    MDNS.begin(Configuration._hostname.c_str());
+    MDNS.addService("http", "tcp", 80);
 
-  MDNS.addService("ftp", "tcp", 21);
-#endif
+    _webServer.enableCORS();
 
-  _webServer.on("/restart", [&]() {
-    _webServer.sendHeader("Access-Control-Allow-Origin", "*");
-    _webServer.send(200, "text/plain", "ESP restart now !");
-    delay(1000);
-    ESP.restart();
-  });
+    _webServer.on("/restart", [&]() {
+        _webServer.send(200, "text/plain", "ESP restart now !");
+        delay(1000);
+        ESP.restart();
+    });
 
-  _webServer.on("/reset", [&]() {
-    _webServer.sendHeader("Access-Control-Allow-Origin", "*");
-    _webServer.send(200, "text/plain", "Reset WifiManager configuration, restart now in AP mode...");
-    delay(200);
-    WiFiManager wifiManager;
-    wifiManager.resetSettings();
-    delay(200);
-    ESP.restart();
-  });
+    _webServer.on("/reset", [&]() {
+        _webServer.send(200, "text/plain", "Reset WifiManager configuration, restart now in AP mode...");
+        delay(200);
+        WiFiManager wifiManager;
+        wifiManager.resetSettings();
+        delay(200);
+        ESP.restart();
+    });
 
-  _webServer.on("/config", HTTP_GET, [&]() { getConfig(); });
-  _webServer.on("/config", HTTP_POST, [&]() { setConfig(); });
-  _webServer.on("/status", HTTP_GET, [&]() { getStatus(); });
-  _webServer.on("/set", [&]() { handleSet(); });
+    _webServer.on("/config", HTTP_GET, [&]() {
+        getConfig();
+    });
+    _webServer.on("/config", HTTP_POST, [&]() {
+        setConfig();
+    });
+    _webServer.on("/status", HTTP_GET, [&]() {
+        getStatus();
+    });
+    _webServer.on("/set", [&]() {
+        handleSet();
+    });
 
-  _webServer.onNotFound([&]() { handleNotFound(); });
+    _webServer.onNotFound([&]() {
+        handleNotFound();
+    });
 
-  _httpUpdater.setup(&_webServer, String("/update"));
-  _webServer.begin();
+    _httpUpdater.setup(&_webServer, String("/update"));
+    _webServer.begin();
 }
 
 void HttpServer::handle(void)
 {
-  _webServer.handleClient();
+    _webServer.handleClient();
 
 #if defined(ESP8266)
-  MDNS.update();
+    MDNS.update();
 #endif
 }
 
 String HttpServer::getContentType(String filename)
 {
-  if (_webServer.hasArg("download"))
-    return "application/octet-stream";
-  else if (filename.endsWith(".htm"))
-    return "text/html";
-  else if (filename.endsWith(".html"))
-    return "text/html";
-  else if (filename.endsWith(".css"))
-    return "text/css";
-  else if (filename.endsWith(".js"))
-    return "application/javascript";
-  else if (filename.endsWith(".png"))
-    return "image/png";
-  else if (filename.endsWith(".gif"))
-    return "image/gif";
-  else if (filename.endsWith(".jpg"))
-    return "image/jpeg";
-  else if (filename.endsWith(".ico"))
-    return "image/x-icon";
-  else if (filename.endsWith(".xml"))
-    return "text/xml";
-  else if (filename.endsWith(".pdf"))
-    return "application/x-pdf";
-  else if (filename.endsWith(".zip"))
-    return "application/x-zip";
-  else if (filename.endsWith(".gz"))
-    return "application/x-gzip";
-  return "text/plain";
+    if (_webServer.hasArg("download"))
+        return "application/octet-stream";
+    else if (filename.endsWith(".htm"))
+        return "text/html";
+    else if (filename.endsWith(".html"))
+        return "text/html";
+    else if (filename.endsWith(".css"))
+        return "text/css";
+    else if (filename.endsWith(".js"))
+        return "application/javascript";
+    else if (filename.endsWith(".png"))
+        return "image/png";
+    else if (filename.endsWith(".gif"))
+        return "image/gif";
+    else if (filename.endsWith(".jpg"))
+        return "image/jpeg";
+    else if (filename.endsWith(".ico"))
+        return "image/x-icon";
+    else if (filename.endsWith(".xml"))
+        return "text/xml";
+    else if (filename.endsWith(".pdf"))
+        return "application/x-pdf";
+    else if (filename.endsWith(".zip"))
+        return "application/x-zip";
+    else if (filename.endsWith(".gz"))
+        return "application/x-gzip";
+    else if (filename.endsWith(".json"))
+        return "application/json";
+    return "text/plain";
 }
 
 // send the right file to the client (if it exists)
 bool HttpServer::handleFileRead(String path)
 {
-  Log.println("handleFileRead: " + path);
-  if (path.endsWith(F("/")))
-    path += "index.html"; // If a folder is requested, send the index file
+    // Send cors and exit properly when method OPTIONS
+    if (_webServer.method() == HTTP_OPTIONS)
+    {
+        _webServer.send(204);
+        return true;
+    }
 
-  String contentType = getContentType(path); // Get the MIME type
+    if (path.endsWith(F("/")))
+        path += "index.html"; // If a folder is requested, send the index file
 
-  String pathWithGz = path + F(".gz");
-  if (FS.exists(pathWithGz)) // If there's a compressed version available
-    path += ".gz";                 // Use the compressed verion
+    Log.println("handleFileRead: " + path);
 
-  if (FS.exists(path))
-  {
-    File file = FS.open(path, "r"); // Open the file
-    _webServer.sendHeader("Access-Control-Allow-Origin", "*");
-    _webServer.streamFile(file, contentType); // Send it to the client
-    file.close();                             // Close the file again
-    Log.println(String("\tSent file: ") + path);
-    return true;
-  }
-  Log.println(String("\tFile Not Found: ") + path); // If the file doesn't exist, return false
-  return false;
+    // Check if file is on flash and send it
+    if (checkAndSendFile(path, _webServer))
+        return true;
+
+    String contentType = getContentType(path); // Get the MIME type
+    String pathWithGz = path + F(".gz");
+    if (FS.exists(pathWithGz)) // If there's a compressed version available
+        path += ".gz";         // Use the compressed verion
+
+    if (FS.exists(path))
+    {
+        File file = FS.open(path, "r"); // Open the file
+        _webServer.streamFile(file, contentType); // Send it to the client
+        file.close();                             // Close the file again
+        Log.println(String("\tSent file: ") + path);
+        return true;
+    }
+    Log.println(String("\tFile Not Found: ") + path); // If the file doesn't exist, return false
+    return false;
 }
 
 void HttpServer::handleNotFound()
 {
-  if (!handleFileRead(_webServer.uri()))
-  {
-    String message = "File Not Found\n\n";
-    message += "URI: ";
-    message += HTTPServer.webServer().uri();
-    message += "\nMethod: ";
-    message += (HTTPServer.webServer().method() == HTTP_GET) ? "GET" : "POST";
-    message += "\nArguments: ";
-    message += HTTPServer.webServer().args();
-    message += "\n";
-    for (uint8_t i = 0; i < HTTPServer.webServer().args(); i++)
+    if (!handleFileRead(_webServer.uri()))
     {
-      message += " " + HTTPServer.webServer().argName(i) + ": " + HTTPServer.webServer().arg(i) + "\n";
+        String message = "File Not Found\n\n";
+        message += "URI: ";
+        message += HTTPServer.webServer().uri();
+        message += "\nMethod: ";
+        message += (HTTPServer.webServer().method() == HTTP_GET) ? "GET" : "POST";
+        message += "\nArguments: ";
+        message += HTTPServer.webServer().args();
+        message += "\n";
+        for (uint8_t i = 0; i < HTTPServer.webServer().args(); i++)
+        {
+            message += " " + HTTPServer.webServer().argName(i) + ": " + HTTPServer.webServer().arg(i) + "\n";
+        }
+        HTTPServer.webServer().send(404, "text/plain", message);
     }
-    HTTPServer.webServer().send(404, "text/plain", message);
-  }
 }
 
 void HttpServer::getStatus()
 {
-  Log.println("Send Status to HTTP");
+    Log.println("Send Status to HTTP");
 
-  DynamicJsonDocument doc(1024);
-  doc.clear();
+    DynamicJsonDocument doc(1024);
+    doc.clear();
 
-  doc["version"] = String(VERSION);
-  doc["build"] = String(BUILD_DATE);
-  doc["relay"] = digitalRead(RELAY_PIN);
+    doc["version"] = String(VERSION);
+    doc["build"] = String(BUILD_DATE);
+    doc["relay"] = digitalRead(RELAY_PIN);
 
-  JsonObject jsonLineA = doc.createNestedObject("lineA");
-  metering *line = Monitoring.getLineA();
-  jsonLineA["voltage"] = line->voltage;
-  jsonLineA["current"] = line->current;
-  jsonLineA["power"] = line->power;
-  jsonLineA["cosPhy"] = line->cosPhy;
-  jsonLineA["conso"] = line->conso;
+    JsonObject jsonLineA = doc.createNestedObject("lineA");
+    metering *line = Monitoring.getLineA();
+    jsonLineA["voltage"] = line->voltage;
+    jsonLineA["current"] = line->current;
+    jsonLineA["power"] = line->power;
+    jsonLineA["cosPhy"] = line->cosPhy;
+    jsonLineA["conso"] = line->conso;
 
-  JsonObject jsonLineB = doc.createNestedObject("lineB");
-  line = Monitoring.getLineB();
-  jsonLineB["voltage"] = line->voltage;
-  jsonLineB["current"] = line->current;
-  jsonLineB["power"] = line->power;
-  jsonLineB["cosPhy"] = line->cosPhy;
-  jsonLineB["conso"] = line->conso;
+    JsonObject jsonLineB = doc.createNestedObject("lineB");
+    line = Monitoring.getLineB();
+    jsonLineB["voltage"] = line->voltage;
+    jsonLineB["current"] = line->current;
+    jsonLineB["power"] = line->power;
+    jsonLineB["cosPhy"] = line->cosPhy;
+    jsonLineB["conso"] = line->conso;
 
-  JsonObject jsonLineC = doc.createNestedObject("lineC");
-  line = Monitoring.getLineC();
-  jsonLineC["voltage"] = line->voltage;
-  jsonLineC["current"] = line->current;
-  jsonLineC["power"] = line->power;
-  jsonLineC["cosPhy"] = line->cosPhy;
-  jsonLineC["conso"] = line->conso;
+    JsonObject jsonLineC = doc.createNestedObject("lineC");
+    line = Monitoring.getLineC();
+    jsonLineC["voltage"] = line->voltage;
+    jsonLineC["current"] = line->current;
+    jsonLineC["power"] = line->power;
+    jsonLineC["cosPhy"] = line->cosPhy;
+    jsonLineC["conso"] = line->conso;
 
-  // Send Status
-  HTTPServer.sendJson(200, doc);
+    // Send Status
+    HTTPServer.sendJson(200, doc);
 }
 
 void HttpServer::getConfig()
 {
-  Log.println("Send Configuration");
+    Log.println("Send Configuration");
 
-  DynamicJsonDocument doc(1024);
-  Configuration.encodeToJson(doc);
+    DynamicJsonDocument doc(1024);
+    Configuration.encodeToJson(doc);
 
-  // Send Configuration
-  sendJson(200, doc);
+    // Send Configuration
+    sendJson(200, doc);
 }
 
 void HttpServer::setConfig()
 {
-  if (_webServer.hasArg("plain") == false)
-  {
-    Log.println("Error, no body received !");
-    _webServer.sendHeader("Access-Control-Allow-Origin", "*");
-    _webServer.send(404, "text/plain", "Body not received");
-  }
-  else
-  {
-    Log.println("Received new configuration !");
-    Log.println(_webServer.arg("plain"));
-    if (!Configuration.decodeJsonFromFile(_webServer.arg("plain").c_str()))
+    if (_webServer.hasArg("plain") == false)
     {
-      Configuration.print();
-      _webServer.sendHeader("Access-Control-Allow-Origin", "*");
-      Monitoring.setConsoLineA(Configuration._consoA);
-      Monitoring.setConsoLineB(Configuration._consoB);
-      Monitoring.setConsoLineC(Configuration._consoC);
-      relay.setTimeout(Configuration._timeoutRelay);
-      if (Configuration.saveConfig())
-      {
-        // _webServer.send(200, "application/json", Configuration.encodeToJson());
-        _webServer.send(200, "application/json", "{\"result\":true}");
-      }
-      else
-      {
-        _webServer.send(200, "application/json", "{\"result\":false}");
-      }
+        Log.println("Error, no body received !");
+        _webServer.send(404, "text/plain", "Body not received");
     }
     else
     {
-      Log.println("Error, parsing JSON !");
-      _webServer.sendHeader("Access-Control-Allow-Origin", "*");
-      _webServer.send(404, "text/plain", "Error with parsing JSON");
+        Log.println("Received new configuration !");
+        Log.println(_webServer.arg("plain"));
+        if (!Configuration.decodeJsonFromFile(_webServer.arg("plain").c_str()))
+        {
+            Configuration.print();
+            Monitoring.setConsoLineA(Configuration._consoA);
+            Monitoring.setConsoLineB(Configuration._consoB);
+            Monitoring.setConsoLineC(Configuration._consoC);
+            relay.setTimeout(Configuration._timeoutRelay);
+            if (Configuration.saveConfig())
+            {
+                // _webServer.send(200, "application/json", Configuration.encodeToJson());
+                _webServer.send(200, "application/json", "{\"result\":true}");
+            }
+            else
+            {
+                _webServer.send(200, "application/json", "{\"result\":false}");
+            }
+        }
+        else
+        {
+            Log.println("Error, parsing JSON !");
+            _webServer.send(404, "text/plain", "Error with parsing JSON");
+        }
     }
-  }
 }
 
 void HttpServer::handleSet()
 {
-  String message;
-  for (uint8_t i = 0; i < HTTPServer.webServer().args(); ++i)
-  {
-    String name = HTTPServer.webServer().argName(i);
-    String value = HTTPServer.webServer().arg(i).c_str();
-    if (String(name) == "relay")
+    String message;
+    for (uint8_t i = 0; i < HTTPServer.webServer().args(); ++i)
     {
-      int state = value.toInt();
-      relay.setState(state);
-      message += "Set Relay to " + String(state) + "\n";
+        String name = HTTPServer.webServer().argName(i);
+        String value = HTTPServer.webServer().arg(i).c_str();
+        if (String(name) == "relay")
+        {
+            int state = value.toInt();
+            relay.setState(state);
+            message += "Set Relay to " + String(state) + "\n";
+        }
+        else if (String(name) == "timeIntervalUpdate")
+        {
+            int time = value.toInt();
+            Log.println(String("set timeSendData to ") + String(time));
+            Configuration._timeSendData = time;
+            Configuration.saveConfig();
+            MqttClient.publish(String("/timeIntervalUpdate"), String(Configuration._timeSendData));
+            message += "Set time Send Data to " + String(time) + "\n";
+        }
+        else if (String(name) == "mode")
+        {
+            int mode = value.toInt();
+            Log.println(String("set mode to ") + String(mode));
+            Configuration._mode = mode;
+            Configuration.saveConfig();
+            MqttClient.publish(String("/mode"), String(Configuration._mode));
+            message += "Set Mode to " + String(mode) + "\n";
+        }
+        else if (String(name) == "hostname")
+        {
+            String hostname = value;
+            Log.println("Change hostname to " + hostname);
+            Configuration._hostname = hostname;
+            Configuration.saveConfig();
+            message += "Set hostname to " + String(hostname) + "\n";
+        }
+        else if (String(name) == "consoA")
+        {
+            int conso = value.toInt();
+            Log.println("Set conso A to " + String(conso));
+            Monitoring.setConsoLineA(conso);
+            message += "Set consoA to " + String(conso) + "\n";
+        }
+        else if (String(name) == "consoB")
+        {
+            int conso = value.toInt();
+            Log.println("Set conso B to " + String(conso));
+            Monitoring.setConsoLineB(conso);
+            message += "Set consoB to " + String(conso) + "\n";
+        }
+        else if (String(name) == "consoC")
+        {
+            int conso = value.toInt();
+            Log.println("Set conso C to " + String(conso));
+            Monitoring.setConsoLineC(conso);
+            message += "Set consoC to " + String(conso) + "\n";
+        }
     }
-    else if (String(name) == "timeIntervalUpdate")
-    {
-      int time = value.toInt();
-      Log.println(String("set timeSendData to ") + String(time));
-      Configuration._timeSendData = time;
-      Configuration.saveConfig();
-      MqttClient.publish(String("/timeIntervalUpdate"), String(Configuration._timeSendData));
-      message += "Set time Send Data to " + String(time) + "\n";
-    }
-    else if (String(name) == "mode")
-    {
-      int mode = value.toInt();
-      Log.println(String("set mode to ") + String(mode));
-      Configuration._mode = mode;
-      Configuration.saveConfig();
-      MqttClient.publish(String("/mode"), String(Configuration._mode));
-      message += "Set Mode to " + String(mode) + "\n";
-    }
-    else if (String(name) == "hostname")
-    {
-      String hostname = value;
-      Log.println("Change hostname to " + hostname);
-      Configuration._hostname = hostname;
-      Configuration.saveConfig();
-      message += "Set hostname to " + String(hostname) + "\n";
-    }
-    else if (String(name) == "consoA")
-    {
-      int conso = value.toInt();
-      Log.println("Set conso A to " + String(conso));
-      Monitoring.setConsoLineA(conso);
-      message += "Set consoA to " + String(conso) + "\n";
-    }
-    else if (String(name) == "consoB")
-    {
-      int conso = value.toInt();
-      Log.println("Set conso B to " + String(conso));
-      Monitoring.setConsoLineB(conso);
-      message += "Set consoB to " + String(conso) + "\n";
-    }
-    else if (String(name) == "consoC")
-    {
-      int conso = value.toInt();
-      Log.println("Set conso C to " + String(conso));
-      Monitoring.setConsoLineC(conso);
-      message += "Set consoC to " + String(conso) + "\n";
-    }
-  }
 
-  HTTPServer.webServer().sendHeader("Access-Control-Allow-Origin", "*");
-  HTTPServer.webServer().send(200, "text/plain", message);
+    HTTPServer.webServer().send(200, "text/plain", message);
 }
-
 
 /********************************************************/
 /******************** Private Method ********************/
@@ -316,21 +332,21 @@ void HttpServer::handleSet()
 
 void HttpServer::sendJson(const uint16_t code, JsonDocument &doc)
 {
-  WiFiClient client = _webServer.client();
+    WiFiClient client = _webServer.client();
 
-  // Write Header
-  client.print(F("HTTP/1.0 "));
-  client.print(code);
-  client.println(F(" OK"));
-  client.println(F("Content-Type: application/json"));
-  client.println(F("Access-Control-Allow-Origin: *"));
-  client.print(F("Content-Length: "));
-  client.println(measureJson(doc));
-  client.println(F("Connection: close"));
-  client.println();
+    // Write Header
+    client.print(F("HTTP/1.0 "));
+    client.print(code);
+    client.println(F(" OK"));
+    client.println(F("Content-Type: application/json"));
+    client.println(F("Access-Control-Allow-Origin: *"));
+    client.print(F("Content-Length: "));
+    client.println(measureJson(doc));
+    client.println(F("Connection: close"));
+    client.println();
 
-  // Write JSON document
-  serializeJson(doc, client);
+    // Write JSON document
+    serializeJson(doc, client);
 }
 
 #if !defined(NO_GLOBAL_INSTANCES)
